3a2a3d06e8c233f93124623b9ab197fa6332f783
Tim <randomthecoder1557@gmail.com>
2025-10-14 09:52:46 -0300

    initialize menu from `main.ts`

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
 import { argv } from "node:process";
 import entryFile from "./models/entryFile";
+import { startMenu } from "./models/Menu";
+import { parseGraph } from "./graph/Parser";
 
-function main() {
+async function main() {
   const filePath = argv[2];
+  const format = argv[3];
 
-  entryFile.read(filePath);
+  const rawGraph = entryFile.read(filePath);
+  if (!rawGraph) return;
+
+  await startMenu(parseGraph(rawGraph, format), format);
 }
 
 main();
